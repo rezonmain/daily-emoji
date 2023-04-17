0cf3aea065967e01c9344ed3ced2c4f5de77c40f
rezonmain <adiazgomez101@gmail.com>
2023-04-17 00:03:25 -0700

    adds: latest post

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,5 +1,19 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { Spinner } from "~/components/Spinner";
+import { api } from "~/utils/api";
+
+const LatestPost = () => {
+  const { data, isLoading, error } = api.posts.getLatest.useQuery();
+  if (isLoading) return <Spinner size={22} />;
+  if (error) return <p className="text-red-400">Something went wrong</p>;
+  return (
+    <p className="text-white">
+      Currently showing:{" "}
+      <pre className=" text-center text-2xl">{data?.content}</pre>
+    </p>
+  );
+};
 
 const Page: NextPage = () => {
   return (
@@ -10,6 +24,7 @@ const Page: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <h1 className="text-4xl font-bold text-white">Post</h1>
+        <LatestPost />
       </main>
     </>
   );
