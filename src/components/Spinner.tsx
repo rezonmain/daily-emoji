65a017ae70fc94d4952f0d81aae4481f5628f76e
rezonmain <adiazgomez101@gmail.com>
2023-04-17 02:45:02 -0700

    update: spinner style

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@ export const Spinner = (props: { size?: number }) => (
   <div role="status">
     <svg
       aria-hidden="true"
-      className="animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+      className="animate-spin fill-slate-100 text-gray-200 dark:text-gray-600"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
