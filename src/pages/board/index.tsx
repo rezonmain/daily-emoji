6f5ac48feaf7da5b36596810b8411f61bdce8f6d
rezonmain <adiazgomez101@gmail.com>
2023-04-17 02:45:15 -0700

    update: board page favicon

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -18,7 +18,10 @@ const Page: NextPage = () => {
     <>
       <Head>
         <title>Daily Emoji | Board</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌯</text></svg>"
+        />
       </Head>
       <LatestPost />
     </>
