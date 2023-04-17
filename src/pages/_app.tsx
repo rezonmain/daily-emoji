c3ab5dd43d011a661fe41742631c831766eed833
rezonmain <adiazgomez101@gmail.com>
2023-04-17 01:05:37 -0700

    adds: toasts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { type AppType } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Toaster />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
